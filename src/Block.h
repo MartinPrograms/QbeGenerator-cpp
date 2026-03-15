e436a7896be8166f2ac176afb61c179786c0cdab
Martin <36630572+MartinPrograms@users.noreply.github.com>
2026-03-15 21:56:21 +0100

    Fix compilation errors on macos

--- src/Block.h
+++ src/Block.h
@@ -244,13 +244,13 @@ namespace Qbe {
         }
 
         ValueReference allocateType(CustomType* type, bool is64Bit) {
-            auto size = addCopy(ValueReference(new Literal(static_cast<ITypeDefinition*>(type)->ByteSize(is64Bit), true))); // Force a 64-bit literal for size
+            auto size = addCopy(ValueReference(new Literal((int64_t)static_cast<ITypeDefinition*>(type)->ByteSize(is64Bit), true))); // Force a 64-bit literal for size
             return addAllocate(size, 4);
         }
 
         ValueReference allocateType(CustomType* type, ValueReference count, bool is64Bit) {
             auto typeSize = static_cast<ITypeDefinition*>(type)->ByteSize(is64Bit);
-            auto totalSize = addMul(count, addCopy(ValueReference(new Literal(typeSize, true)))); // Force a 64-bit literal for size
+            auto totalSize = addMul(count, addCopy(ValueReference(new Literal((int64_t)typeSize, true)))); // Force a 64-bit literal for size
             return addAllocate(totalSize, 4);
         }
 
